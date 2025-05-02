920c25f614c6c0375f781be941214294a9763bd8
Fabryv <39920892+Fabry200@users.noreply.github.com>
2025-05-02 10:52:41 +0200

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,6 +1,5 @@
 from numpy import linalg as LA
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 import math
 import warnings
 import pandas as pd
